5f3118534b0cba59d0ae39f7a63e1277cce3dfda
yStasiv <Yurastasiv@hotmail.com>
2017-06-22 14:18:20 +0300

    внесена поправка формату

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function Fact(){ // факторіал числа через рекурсію
-var n = prompt('Введіть число щоб порахувати факторіал', '1');
+var n = +prompt('Введіть число щоб порахувати факторіал', '1');
 function factorial(n) {
 if(n ==null) alert("error '404'");
 if(n == 0) return 0; if(n == 1) return 1;
